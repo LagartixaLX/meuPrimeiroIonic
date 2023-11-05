f6e14fbc67ef5884fe855d719f5173f19dda9003
Lagartixa <joaops2go@hotmail.com>
2023-11-05 18:23:42 -0300

    criando tela de login na homepage puxando loginpage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {LoadingController, ToastController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public toastController:ToastController,
+              public loadingController:LoadingController,
+              private router:Router)
+              {
+                //this.router.navigate(['/login'])
+              }
+
+  user_name:any='';
+  pass_word:any='';
+
+
+  async showToast(msg,color)
+  {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      color:color
+    });
+    toast.present();
+  }
+
+  async login_page() {
+    if (this.user_name.length == 0) {
+      this.showToast("Insira o Usuario", "danger");
+    }
+    else if (this.pass_word.length == 0) {
+      this.showToast("Insira sua senha.", "danger");
+    }
+    else {
+          if((this.user_name == "joao") && (this.pass_word == "123")){
+            this.router.navigate(['/login'])
+          }
+          else{
+            this.showToast("Usuario ou senha Incorreto", "danger");
+          }
+    }
+  }
 
 }
