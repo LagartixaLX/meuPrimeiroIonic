f1e8da3fd64b2464dc5d22f747c22ee2db75dee9
Lagartixa <joaops2go@hotmail.com>
2024-03-26 11:32:52 -0300

    :fire: Tela de login interligada com o firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email: string;
+  password: string;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   login(email: string, password: string) {
     this.authService.login(email, password)
       .then(() => {
         console.log('Login bem-sucedido');
         // Redirecionar para a próxima página após o login
+        this.router.navigate(['/login']);
+        
       })
       .catch(error => {
         console.error('Erro de login:', error);
       });
+     
   }
+   
 }
